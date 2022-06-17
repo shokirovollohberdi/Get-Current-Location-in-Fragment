7b8ce4cc6a0b6995d11ba19f0fdfa2b49d366ea8
Shokirov <ollohberdiinfo@gmail.com>
2022-06-17 10:01:55 +0300

    Get current location

---
+++ app/src/main/java/uz/shokirov/getcurrentlocation/BlankFragment.kt
@@ -0,0 +1,91 @@
+package uz.shokirov.getcurrentlocation
+
+import android.annotation.SuppressLint
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
+import uz.shokirov.getcurrentlocation.databinding.FragmentBlankBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment : Fragment() {
+    lateinit var binding: FragmentBlankBinding
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBlankBinding.inflate(layoutInflater)
+
+
+
+        binding.btn.setOnClickListener {
+            currentLocation()
+        }
+
+        return binding.root
+    }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun currentLocation() {
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        val task = fusedLocationProviderClient.lastLocation
+
+        task.addOnSuccessListener {
+            if (it!=null){
+                binding.tv.text = "Lat ${it!!.latitude}\nLong ${it!!.longitude}"
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BlankFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
